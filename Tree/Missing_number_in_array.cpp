e644262bfeba5715c176fa7b759a2d148e5fa087
Komal Singh <komal17354@gmail.com>
2019-03-12 20:01:20 +0530

    Questions related to tree

---
+++ Tree/Missing_number_in_array.cpp
@@ -0,0 +1,24 @@
+//Given an array C of size N-1 and given that there are numbers from 1 to N with one element missing,
+// the missing number is to be found(Give the most Optimized solution)
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	 int i,n,sum=0,sum1;
+	 cin>>n;
+	 int a[n-1];
+	 for(i=0;i<n-1;i++)
+	 cin>>a[i];
+	 sum=n*(n+1)/2;
+	 for(i=0;i<n-1;i++)
+	 {
+	 	sum1=sum-a[i];
+	}
+	cout<<sum1<<endl;	
+	}
+	return 0;
+}
